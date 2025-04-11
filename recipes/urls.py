36fe504d343afdb6b5d2c81affe0734ad01f600c
Guilherme Paleari <guipaleari@outlook.com>
2025-04-11 15:19:05 -0300

    Atualização 11/04/25

--- recipes/urls.py
+++ recipes/urls.py
@@ -1,7 +1,18 @@
 from django.urls import path
 
-from recipes.views import home
+from recipes import views
+
+# Usado para quando der um nome a URL
+# utiliza app_name para simplificar o atributo name logo a baixo de
+# ex: name="recipes-home" para name="home"
+# e depois coloca no template na url da tag "a" dessa forma -> recipes:home
+app_name = "recipes"
 
 urlpatterns = [
-    path('', home),
+    path("", views.home, name="home"),
+    path("recipes/search/", views.search, name="search"),
+    path(
+        "recipes/category/<int:category_id>/", views.category, name="category"
+    ),
+    path("recipes/<int:id>/", views.recipe, name="recipe"),
 ]
