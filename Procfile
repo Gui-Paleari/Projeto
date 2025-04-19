7fd933a1d549413863fe9f3e58caf746bc9ac96b
Guilherme Paleari <guipaleari@outlook.com>
2025-04-19 12:34:49 -0300

    Atualizacao-19-04-25

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn Project.wsgi
+web: gunicorn project.wsgi
--- project/settings.py
+++ project/settings.py
@@ -1,7 +1,6 @@
 import os
 from pathlib import Path
 
-import dj_database_url
 from django.contrib.messages import constants
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
